abbd6cbe57036775fe3b5bd73efd84c42b9104cc
sitiyoh <aegis7ky@gmail.com>
2020-11-30 22:51:05 +0900

    jpgファイル読み込めてなかったのを修正

--- image.py
+++ image.py
@@ -1,16 +1,19 @@
 import discord
+import os
 import random
 
 class Image:
-    __image_path = "image/BrahmanTrident{0}.png"
+    __image_path = "image/BrahmanTrident{0}"
+    __image_ext = [".jpg", ".png"]
     __file_count = 5
     __file_path_list = []
 
     def __init__(self):
         for i in range(1, self.__file_count + 1):
             path = self.__image_path.format(i)
-            print(path)
-            self.__file_path_list.append(path)
+            for ext in self.__image_ext:
+                if os.path.exists(path + ext):
+                    self.__file_path_list.append(path + ext)
 
     def File(self):
         return discord.File(random.choice(self.__file_path_list))
--- launcher.py
+++ launcher.py
@@ -16,9 +16,14 @@ async def on_message(message):
 
     reaction = Reply.React(message.content)
 
+    if reaction is None:
+        return
+
+    print("{0} {1} {2}:{3} {4}".format(message.content, message.author, message.guild, message.channel, message.created_at))
+
     if type(reaction) is str:
         await message.channel.send(reaction)
-    elif type(reaction) is not None:
+    else:
         await message.channel.send(file=reaction.File())
 
 bot.run(os.environ['KienhoujinToken'])
